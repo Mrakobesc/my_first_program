cf6344fbcf9c8bc9c893d428245ab281d82f8e73
Mark <markazar06@gmail.com>
2024-02-17 16:57:18 +0300

    нахождение наибольшего числа

---
+++ 006/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 9;
+int b = 10;
+int c = 3;
+int d = 20;
+int e = 4;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
