b9a0e32d558d1eabbf72cbfd52f69aa9c8b9d135
Mark <markazar06@gmail.com>
2024-02-23 01:46:06 +0300

    Factorial

---
+++ 015/Program.cs
@@ -0,0 +1,16 @@
+﻿//int Factorial(int n)
+//{
+   // if(n == 1) return 1;
+   // else return n * Factorial(n-1);
+//}
+//System.Console.WriteLine(Factorial(3));
+
+double Factorial(int n) // тут double,а не int , потому что слишком большие числа и int не выводит просто все
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+    System.Console.WriteLine($"{i}! = {Factorial(i)}");
+}
